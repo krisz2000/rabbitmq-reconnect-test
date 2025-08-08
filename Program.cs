be6f1c343e7a4ac811882dde9a8245cebbad7d93
Schlepp Krisztián <krisztian.schlepp@egroup.hu>
2025-08-08 15:57:10 +0200

    add example code

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+
+ConnectionFactory factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest",
+    ClientProvidedName = "test",
+    AutomaticRecoveryEnabled = true,
+};
+
+var initialized = false;
+while (!initialized)
+{
+    try
+    {
+        IConnection conn = await factory.CreateConnectionAsync();
+        initialized = true;
+    }
+    catch (RabbitMQ.Client.Exceptions.BrokerUnreachableException e)
+    {
+        Console.WriteLine($"Error: {e}");
+        await Task.Delay(5000);
+    }
+}
+Console.WriteLine("Connected to rabbitmq");
+Console.ReadKey(); // wait until we can observe connections in the management ui
